d301e4f4e7773de3a77d1e9dd7b2fc0bcb6a4c69
Uwe Sölen Flaig <uwe.soelen.flaig@gmail.com>
2021-04-18 20:00:44 +0200

    Fixed typo.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          The famouse Faisal App!
+          The famous Faisal App!
         </p>
         <a
           className="App-link"
